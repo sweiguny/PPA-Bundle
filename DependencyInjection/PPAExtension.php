5923566b165215d00f339679759f557d59ad6950
sweiguny <s.weiguny@gmail.com>
2019-01-25 16:12:17 +0100

    making bundle usable

--- DependencyInjection/PPAExtension.php
+++ DependencyInjection/PPAExtension.php
@@ -2,17 +2,17 @@
 
 namespace PPA\Bundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\Config\FileLocator;
 
 class PPAExtension extends Extension
 {
     
     public function load(array $configs, ContainerBuilder $container)
     {
-        var_dump($configs);
+//        var_dump($configs);
         
         $loader = new YamlFileLoader(
             $container,
@@ -24,6 +24,16 @@ class PPAExtension extends Extension
         $configuration = new Configuration();
 
         $config = $this->processConfiguration($configuration, $configs);
+        
+        
+        $definition = $container->getDefinition("ppa.connection");
+        $definition->setArgument(1, $config["dbal"]["db_type"]);
+        $definition->setArgument(2, []);
+        $definition->setArgument(3, $config["dbal"]["db_user"]);
+        $definition->setArgument(4, $config["dbal"]["db_password"]);
+        $definition->setArgument(5, $config["dbal"]["db_host"]);
+        $definition->setArgument(6, $config["dbal"]["db_name"]);
+        $definition->addMethodCall("connect");
     }
     
 }
